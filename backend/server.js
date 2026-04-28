512957f4354021d08e175960638f0fb57457fd0b
Muhammed_Yaseen.M <yaseenca235@gmail.com>
2026-04-28 18:20:09 +0530

    ivote

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://ivote-system.onrender.com/'], credentials: true }));
 app.use(express.json());
 
 // Routes
